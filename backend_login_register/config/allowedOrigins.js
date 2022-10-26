44a11f71d73b8abdf9c4b8553b8f4cc2e0c23289
Faiz Rosli <faiz_rsl@hotmail.com>
2022-10-26 21:09:49 +0800

    deployment

--- backend_login_register/config/allowedOrigins.js
+++ backend_login_register/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
-    'https://ezhousewebsite.vercel.app/',
-    'http://ezhousewebsite.vercel.app/',
+    'https://ezhousewebsite.vercel.app',
+    'http://ezhousewebsite.vercel.app',
+    'ezhousewebsite.vercel.app',
+    'www.ezhousewebsite.vercel.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
